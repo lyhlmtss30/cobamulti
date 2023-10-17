45c7c7d7448aa55847771bd6e79a50f54e80b4c8
dany <mroihandany@gmail.com>
2023-10-17 11:22:46 +0700

    regitser

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,19 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
      <!-- Font Icon -->
     <link rel="stylesheet" href="{{ asset('fonts/material-icon/css/material-design-iconic-font.min.css') }}">
 
     <!-- Main css -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    </head>
-
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
 
 <body>
-   
     <div class="main">
         <section class="signup">
             <div class="container">
@@ -24,38 +22,47 @@
                     <div class="signup-form">
                         <h2 class="form-title">Sign up</h2>
                         <form method="POST" class="register-form" id="register-form" action="{{ route('register') }}">
+                            @csrf <!-- Laravel CSRF protection -->
+
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="name" id="name" placeholder="Your Name" />
                             </div>
+
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
                                 <input type="email" name="email" id="email" placeholder="Your Email" />
                             </div>
+
                             <div class="form-group">
-                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass" id="pass" placeholder="Password" />
+                                <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Password" />
                             </div>
+
                             <div class="form-group">
-                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass"
+                                <label for="password_confirmation"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="password" name="password_confirmation" id="password_confirmation"
                                     placeholder="Repeat your password" />
                             </div>
                            
                             <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit"
-                                    value="Register" />
+                                <button type="submit" class="form-submit">Register</button>
                             </div>
                         </form>
                     </div>
+
                     <div class="signup-image">
-                        <figure><img src="assets/images/signup-image.jpg" alt="sing up image"></figure>
-                        <a href="/login" class="signup-image-link">I am already member</a>
+                        <figure><img src="{{ asset('assets/images/signup-image.jpg') }}" alt="sign up image"></figure>
+                        <a href="/login" class="signup-image-link">I am already a member</a>
                     </div>
                 </div>
             </div>
         </section>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+    </script>
+</body>
+
+</html>
